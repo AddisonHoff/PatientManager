73cecd043ff5f648eb2a3d750e434767658270ed
Addison Hoff <addisonghoff@gmail.com>
2020-11-15 17:30:27 -0700

    install bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,9 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
+//= require bootstrap-sprockets
+require("jquery")
+require("boostrap-sprockets")
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
